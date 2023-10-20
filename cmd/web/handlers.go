0835524c3a0acc0fef7c26ad6181a77e491920f3
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-10-20 20:50:11 +0300

    added a package to handle forms and errors, also refactored to use this package

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/SegniAdebaGodsSon/snippetbox/pkg/forms"
+	"github.com/SegniAdebaGodsSon/snippetbox/pkg/models"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
-
-	"github.com/SegniAdebaGodsSon/snippetbox/pkg/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +44,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
@@ -56,36 +56,19 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	bodyErrors := make(map[string]string)
-	if strings.TrimSpace(title) == "" {
-		bodyErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		bodyErrors["title"] = "This field is too long (maximum is 100 characters)"
-	}
-
-	if strings.TrimSpace(content) == "" {
-		bodyErrors["content"] = "This field cannot be blank"
-	}
-
-	if strings.TrimSpace(expires) == "" {
-		bodyErrors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		bodyErrors["expires"] = "This field is invalid"
-	}
-
-	if len(bodyErrors) > 0 {
+	if !form.Valid() {
 		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: bodyErrors,
-			FormData:   r.PostForm,
+			Form: form,
 		})
 		return
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
