d68e72f378729c7c4cb933652f1e5a0051166aeb
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-11-09 09:45:00 +0300

    used interfaces to mock DB models

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,13 +5,15 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"github.com/golangcollege/sessions"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/golangcollege/sessions"
+
+	"github.com/SegniAdebaGodsSon/snippetbox/pkg/models"
 	"github.com/SegniAdebaGodsSon/snippetbox/pkg/models/mysql"
 	"github.com/fatih/color"
 	_ "github.com/go-sql-driver/mysql"
@@ -22,12 +24,20 @@ type contextKey string
 const contextKeyIsAuthenticated = contextKey("isAuthenticated")
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	session       *sessions.Session
-	snippets      *mysql.SnippetModel
+	errorLog *log.Logger
+	infoLog  *log.Logger
+	session  *sessions.Session
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users         *mysql.UserModel
+	users         interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
