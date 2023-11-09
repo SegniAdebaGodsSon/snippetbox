4a593f79064c47720a7542938ff82c938a955754
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-11-09 19:27:51 +0300

    Added an e2e test for Signup

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -62,7 +62,7 @@ func (f *Form) MinLength(field string, d int) {
 	}
 
 	if utf8.RuneCountInString(value) < d {
-		f.Errors.Add(field, fmt.Sprintf("This field is too shord (minimum is %d characters)", d))
+		f.Errors.Add(field, fmt.Sprintf("This field is too short (minimum is %d characters)", d))
 	}
 }
 
